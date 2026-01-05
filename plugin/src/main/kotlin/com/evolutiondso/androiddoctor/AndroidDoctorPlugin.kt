8a5bc8013bf9f05fe9136fa43f2d12750e0d8615
abrahamcruz <beto_ahm@hotmail.com>
2026-01-04 18:35:18 -0600

    Android Doctor - Fixed bug for Json not being updated.

--- plugin/src/main/kotlin/com/evolutiondso/androiddoctor/AndroidDoctorPlugin.kt
+++ plugin/src/main/kotlin/com/evolutiondso/androiddoctor/AndroidDoctorPlugin.kt
@@ -6,10 +6,15 @@ import org.gradle.api.Project
 class AndroidDoctorPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
-        target.tasks.register("androidDoctorCollect", AndroidDoctorCollectTask::class.java) { task ->
+
+        target.tasks.register(
+            "androidDoctorCollect",
+            AndroidDoctorCollectTask::class.java
+        ) { task ->
             task.reportFile.set(
                 target.layout.buildDirectory.file("androidDoctor/report.json")
             )
+            task.outputs.upToDateWhen { false }
         }
 
         target.logger.lifecycle("AndroidDoctor plugin applied to project: ${target.path}")
